cddb6d7d2b35e2df7c2b79a99240c55015d84361
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-03-04 10:46:48 +0700

    create main page

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,9 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main_page/main-page';
+
+function App () : JSX.Element {
+  return (
+    <MainPage />
+  );
 }
 
 export default App;
