5f3c2fd9daabb4eff2efe46b18296a1c08db922f
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-03-08 12:28:36 +0700

    add 404

---
+++ project/src/pages/not-found/not-found.tsx
@@ -0,0 +1,8 @@
+const NotFound = (): JSX.Element => (
+  <section className = "not_found">
+    <h1>404. Page not found</h1>
+    <Link to="/">Вернуться на главную</Link>
+  </section>
+);
+
+export default NotFound;
