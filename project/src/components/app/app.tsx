909d84689312193f042fc5d2714301c7ad7f730e
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-03-08 13:02:19 +0700

    add route

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,23 @@
 import Main from '../../pages/main/index';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Offer from '../../pages/offer/index';
+import Login from '../../pages/login/index';
+import { AppRoute } from './const';
+import NotFound from '../../pages/not-found';
 
 type AppProps = {
   placesCount: number;
 }
 
 const App = ({ placesCount }: AppProps) => (
-  <Main placesCount={placesCount} />
+  <BrowserRouter>
+    <Routes>
+      <Route path={AppRoute.Root} element={<Main placesCount={placesCount} />} />
+      <Route path={AppRoute.Offer} element={<Offer />} />
+      <Route path={AppRoute.Login} element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
