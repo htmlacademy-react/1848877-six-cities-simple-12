df4d6131b780c77ebdccd3413e91b94cc1da0049
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-03-27 17:58:19 +0700

    fix

--- project/src/components/card-list/card-list.tsx
+++ project/src/components/card-list/card-list.tsx
@@ -1,35 +1,35 @@
 import Card from '../card';
 import { Offers } from '../../types/offers';
-//import { useState } from 'react';
+import { useState } from 'react';
 
 type ListCardProps = {
   offers: Offers[];
-  // onListItemHover?: (listItemName: number | null) => void;
+  onListItemHover?: (listItemName: number | null) => void;
   cardType: 'home' | 'property';
 };
 
 const CardList = ({ offers, cardType }: ListCardProps) => {
-  //const [selectedOfferId, setSelectedOfferId] = useState<number | null>(
-  //null
-  //);
-  //return (
-  <>
-    {offers.map(({ price, previewImage, title, type, isPremium, id, rating }) => (
-      <Card
-        key={id}
-        price={price}
-        previewImage={previewImage}
-        title={title}
-        type={type}
-        isPremium={isPremium}
-        id={id}
-        rating={rating}
-        // onMouseEnter={() => setSelectedOfferId?.(id)}
-        cardType={cardType}
-      />
-    ))}
-  </>;
-  //);
+  const [selectedOfferId, setSelectedOfferId] = useState<number | null>(
+    null
+  );
+  return (
+    <>
+      {offers.map(({ price, previewImage, title, type, isPremium, id, rating }) => (
+        <Card
+          key={id}
+          price={price}
+          previewImage={previewImage}
+          title={title}
+          type={type}
+          isPremium={isPremium}
+          id={id}
+          rating={rating}
+          onMouseEnter={() => setSelectedOfferId?.(id)}
+          cardType={cardType}
+        />
+      ))}
+    </>
+  );
 };
 
 
--- project/src/components/cities/cities.tsx
+++ project/src/components/cities/cities.tsx
@@ -6,33 +6,39 @@ type CitiesProps = {
   onChangeCity: (city: string) => void;
 };
 
-const Cities = ({ currentCity, onChangeCity }: CitiesProps) => (
-  <div className="tabs">
-    <section className="locations container">
-      <ul className="locations__list tabs__list">
-        {CITIES.map((city) => {
-          const className = cn('locations__item-link tabs__item', {
-            'tabs__item--active': currentCity === city,
-          });
-          return (
-            <li className="locations__item" key={city}>
-              <a
-                className={className}
-                href="/#"
-                onClick={(evt) => {
-                  evt.preventDefault();
-                  onChangeCity(city);
-                }}
-              >
-                <span>{city}</span>
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
-  </div>
-);
-
+const Cities = ({ currentCity, onChangeCity }: CitiesProps) => {
+  const handleChangeCity = (
+    evt: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    city: string) => {
+    evt.preventDefault();
+    onChangeCity(city);
+  };
+  return (
+    <div className="tabs">
+      <section className="locations container">
+        <ul className="locations__list tabs__list">
+          {CITIES.map((city) => {
+            const className = cn('locations__item-link tabs__item', {
+              'tabs__item--active': currentCity === city,
+            });
+            return (
+              <li className="locations__item" key={city}>
+                <a
+                  className={className}
+                  href="/#"
+                  onClick={(evt) => {
+                    handleChangeCity(evt, city);
+                  }}
+                >
+                  <span>{city}</span>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </div>
+  );
+};
 
 export default Cities;
