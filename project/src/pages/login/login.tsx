33b98f55b79d6d95ed55b899127100c5eb0206d4
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-04-01 09:35:20 +0700

    fix

--- project/src/pages/login/login.tsx
+++ project/src/pages/login/login.tsx
@@ -74,7 +74,7 @@ const Login = () => {
               <section className="locations locations--login locations--current">
                 <div className="locations__item">
                   <a className="locations__item-link" href="#">
-                    <span>Amsterdam</span>
+                    <span>Paris</span>
                   </a>
                 </div>
               </section>
