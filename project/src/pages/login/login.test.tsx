ef93ca4e2f29a02d9331a3e1f783350c6137ce65
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-04-13 16:47:41 +0700

    delete

--- project/src/pages/login/login.test.tsx
+++ project/src/pages/login/login.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import HistoryRouter from '../../components/history-router';
 import Login from './login';
 import { configureMockStore } from '@jedmao/redux-mock-store';
-import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { AppRoute } from '../../router/RoutePath';
 import { AuthorizationStatus, NameSpace } from '../../constants/constants';
@@ -11,7 +10,7 @@ import { AuthorizationStatus, NameSpace } from '../../constants/constants';
 const mockStore = configureMockStore();
 
 describe('Component: Login', () => {
-  it('should render correctly', async () => {
+  it('should render correctly', () => {
     const history = createMemoryHistory();
     history.push(AppRoute.Login);
     const initialState = {
@@ -30,11 +29,5 @@ describe('Component: Login', () => {
 
     expect(screen.getByLabelText('E-mail')).toBeInTheDocument();
     expect(screen.getByLabelText('Password')).toBeInTheDocument();
-
-    await userEvent.type(screen.getByTestId('login'), 'keks');
-    await userEvent.type(screen.getByTestId('password'), '123456a');
-
-    expect(screen.getByDisplayValue(/keks/i)).toBeInTheDocument();
-    expect(screen.getByDisplayValue(/123456a/i)).toBeInTheDocument();
   });
 });
